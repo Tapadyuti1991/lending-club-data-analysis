4cddfcf74e68dd87c3511a4fe16bc9a9467c7433
Nehal Bhanushali <Nehal Bhanushali>
2017-04-06 20:06:44 -0400

    added merge task to luigi

--- luigi/tasks/merge_accepted_loans.py
+++ luigi/tasks/merge_accepted_loans.py
@@ -4,10 +4,30 @@ class MergeDataDownloaded(luigi.Task):
 
     def run(self):
         # end whtever needs to be run
+        cwd = os.getcwd()
+        folder = os.path.join(cwd,"Data/DOWNLOAD_LOAN_DATA")
+        output_folder = os.path.join(cwd,"Data/")
+        print("Started : Prepare download data files for merge")
+        df_list = []
+        
+        #check inorder to avoid inclusion in merge to be tested to-do $$$$
+        mergedFile = output_folder+"/CombinedDownloadData.csv" #check / \\  >> \
+        if os.path.exists(mergedFile):   
+            os.remove(mergedFile)
+        
+        for filename in os.listdir(folder):
+            df_list.append(pd.read_csv(filename,skiprows=1,low_memory=False))
+            
+        combined_csv = pd.concat(df_list)
+        combined_csv.to_csv(output_folder+"/CombinedDownloadData.csv", index=False ) #check / \\ \
+        
+        print("Finished : Merging download data files to CombinedDownloadData.csv ")
+        
+        print(combined_csv.head())
 
     def output(self):
         #save file to Data directory
-        return luigi.LocalTarget('/Data/')
+        return luigi.LocalTarget('/Data/') ## check to-do $$$
 
 # if __name__ == '__main__':
 #     luigi.run(['HelloWorldTask', '--workers', '1', '--local-scheduler'])
